c7a34d989d44ee2279d6035b91fbd6f3ce1c8718
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-08-06 12:49:28 +0200

    ✨(API) : Create schedule and employee interface for database

--- Front-end/src/app/services/user/user.service.ts
+++ Front-end/src/app/services/user/user.service.ts
@@ -29,5 +29,39 @@ export class UserService {
     return true
   }
 
+  public loadEmployeeData(employees : any [])
+  {
+    for (let k = 0; k < employees.length; k++) {
+      const employeeRef = this.store.collection('users', ref => ref.where('id', '==', employees[k].id));
+      const employee = employeeRef.valueChanges();
+      employee.subscribe((response) => {
+        if (response.length > 0)
+        {
+          this.store.collection("employees").add(employees[k]).catch(
+            () => {alert("An error occured while storing user data")}
+          )
+        }
+      });
+    }
+    return true;
+  }
+
+  public loadScheduleData(schedules : any [])
+  {
+    for (let k = 0; k < schedules.length; k++) {
+      const scheduleRef = this.store.collection('users', ref => ref.where('id', '==', schedules[k].id));
+      const schedule = scheduleRef.valueChanges();
+      schedule.subscribe((response) => {
+        if (response.length > 0)
+        {
+          this.store.collection("schedules").add(schedules[k]).catch(
+            () => {alert("An error occured while storing user data")}
+          )
+        }
+      });
+    }
+    return true;
+  }
+
   
 }
