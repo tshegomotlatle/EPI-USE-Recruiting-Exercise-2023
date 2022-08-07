41d473e8b06e7aa7ee77ef4bbefd1759ef909ad9
Tshegofatsho Motlatle <tshegomotlatle87@gmail.com>
2022-08-07 09:16:35 +0200

    ✨(services) : Added date pipe

---
+++ Front-end/src/app/pipes/date-format.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateFormat'
+})
+export class DateFormatPipe implements PipeTransform {
+
+  transform(value: string): string {
+    console.log();
+    return value.slice(value.indexOf(" "));
+  }
+
+}
